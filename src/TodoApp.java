a09d8ae11e794da641f7b47ba0a7b6d33e483673
hyk4869 <hyk4869@gmail.com>
2024-09-16 13:52:06 +0900

    javaのswingが動くように調整

--- src/TodoApp.java
+++ src/TodoApp.java
@@ -1,9 +1,19 @@
 package src;
 
+import javax.swing.JFrame;
+
 public class TodoApp {
   public static void main(String[] args) {
 
-    var a = 1;
+    JFrame frame = new JFrame("ToDo Application");
+
+    frame.setSize(500, 400);
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    frame.setLocationRelativeTo(null);
+
+    frame.setVisible(true);
 
   }
 }
